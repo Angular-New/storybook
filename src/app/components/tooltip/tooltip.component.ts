af268f08299ed04c2e046c25e44e6a9895347f5f
Verda Muso <gearmobile@gmail.com>
2023-12-04 22:00:02 +0300

    start tooltip

--- src/app/components/tooltip/tooltip.component.ts
+++ src/app/components/tooltip/tooltip.component.ts
@@ -1,11 +1,59 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostBinding,
+  HostListener,
+  Inject,
+  Input,
+  Self,
+  ViewChild
+} from '@angular/core';
+import {Observable, takeUntil} from "rxjs";
 
 @Component({
   selector: 'app-tooltip',
   templateUrl: './tooltip.component.html',
-  styleUrls: ['./tooltip.component.css'],
+  styleUrls: ['./tooltip.component.scss'],
+  providers: [TuiDestroyService, MODE_PROVIDER],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TooltipComponent {
-  //
+  private mode: TuiBrightness | null = null;
+
+  @ViewChild(TuiHintHoverDirective)
+  readonly driver$?: TuiHintHoverDirective;
+
+  @Input()
+  describeId = '';
+
+  @Input()
+  context?: C;
+
+  constructor(
+    @Self() @Inject(TuiDestroyService) destroy$: Observable<unknown>,
+    @Inject(TUI_MODE) mode$: Observable<TuiBrightness | null>,
+    @Inject(TUI_HINT_OPTIONS) options: TuiHintOptions,
+    @Inject(TUI_IS_MOBILE) private readonly isMobile: boolean,
+  ) {
+    super(options);
+
+    mode$.pipe(takeUntil(destroy$)).subscribe(mode => {
+      this.mode = mode;
+    });
+  }
+
+  @HostBinding('attr.data-appearance')
+  get computedAppearance(): string {
+    return this.appearance || this.mode || '';
+  }
+
+  @HostListener('mousedown', ['$event'])
+  stopOnMobile(event: MouseEvent): void {
+    if (this.isMobile) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    this.driver$?.toggle();
+  }
 }
---
+++ src/app/components/tooltip/tooltip.module.ts
@@ -0,0 +1,21 @@
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+import {TuiSvgModule} from '@taiga-ui/core/components/svg';
+import {TuiHintModule} from '@taiga-ui/core/directives/hint';
+import {TuiWrapperModule} from '@taiga-ui/core/directives/wrapper';
+import {PolymorpheusModule} from '@tinkoff/ng-polymorpheus';
+
+import {TuiTooltipComponent} from './tooltip.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    TuiWrapperModule,
+    TuiSvgModule,
+    TuiHintModule,
+    PolymorpheusModule,
+  ],
+  declarations: [TuiTooltipComponent],
+  exports: [TuiTooltipComponent],
+})
+export class TooltipModule {}
