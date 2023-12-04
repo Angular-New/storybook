e1af8bab9d32fb6e035d831451ba4556846b8496
Verda Muso <gearmobile@gmail.com>
2023-12-04 20:45:02 +0300

    create the task component

---
+++ src/app/components/task/task.component.ts
@@ -0,0 +1,39 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { ITask } from "../../models";
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+})
+export default class TaskComponent {
+  /**
+   * The shape of the task object
+   */
+  @Input() task: ITask = <ITask>{};
+
+  // tslint:disable-next-line: no-output-on-prefix
+  @Output()
+  onPinTask: EventEmitter<Event> = new EventEmitter<Event>();
+
+  // tslint:disable-next-line: no-output-on-prefix
+  @Output()
+  onArchiveTask: EventEmitter<Event> = new EventEmitter<Event>();
+
+  /**
+   * @ignore
+   * Component method to trigger the onPin event
+   * @param id string
+   */
+  onPin(id: any): void {
+    this.onPinTask.emit(id);
+  }
+
+  /**
+   * @ignore
+   * Component method to trigger the onArchive event
+   * @param id string
+   */
+  onArchive(id: any): void {
+    this.onArchiveTask.emit();
+  }
+}
